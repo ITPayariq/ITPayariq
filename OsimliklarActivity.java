1bc5f0f6e99fe1af81445726661a2af3b963d86c
ITPayariq <108451577+ITPayariq@users.noreply.github.com>
2022-06-30 11:31:46 +0500

    Add files via upload

---
+++ OsimliklarActivity.java
@@ -0,0 +1,203 @@
+package uz.eko.bolajon.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.*;
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.view.View.*;
+import android.widget.*;
+import android.content.*;
+import android.content.res.*;
+import android.graphics.*;
+import android.graphics.drawable.*;
+import android.media.*;
+import android.net.*;
+import android.text.*;
+import android.text.style.*;
+import android.util.*;
+import android.webkit.*;
+import android.animation.*;
+import android.view.animation.*;
+import java.util.*;
+import java.util.regex.*;
+import java.text.*;
+import org.json.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.content.Intent;
+import android.net.Uri;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.DialogFragment;
+
+
+public class OsimliklarActivity extends  AppCompatActivity  { 
+	
+	
+	private String json_osimliklar = "";
+	
+	private ArrayList<HashMap<String, Object>> lsitmap_osimliklar = new ArrayList<>();
+	
+	private LinearLayout linear1;
+	private ListView listview1;
+	
+	private Intent view_intent = new Intent();
+	@Override
+	protected void onCreate(Bundle _savedInstanceState) {
+		super.onCreate(_savedInstanceState);
+		setContentView(R.layout.osimliklar);
+		initialize(_savedInstanceState);
+		initializeLogic();
+	}
+	
+	private void initialize(Bundle _savedInstanceState) {
+		
+		linear1 = (LinearLayout) findViewById(R.id.linear1);
+		listview1 = (ListView) findViewById(R.id.listview1);
+	}
+	
+	private void initializeLogic() {
+		getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+		getWindow().setStatusBarColor(0xFFFFFFFF);
+		
+		Window window = this.getWindow();window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS); window.setNavigationBarColor(Color.parseColor("#ffffff"));
+		try { 
+			java.io.InputStream is =
+			this.getAssets().open("osimliklar.json");
+			int size = is.available();
+			byte[] buffer = new byte[size];
+			is.read(buffer);
+			is.close();
+			json_osimliklar = new String(buffer, "UTF-8");
+			lsitmap_osimliklar = new Gson().fromJson(json_osimliklar, new TypeToken<ArrayList<HashMap<String, Object>>>(){}.getType());
+			listview1.setAdapter(new Listview1Adapter(lsitmap_osimliklar));
+			((BaseAdapter)listview1.getAdapter()).notifyDataSetChanged();
+		} catch(Exception e) { }
+	}
+	
+	@Override
+	protected void onActivityResult(int _requestCode, int _resultCode, Intent _data) {
+		
+		super.onActivityResult(_requestCode, _resultCode, _data);
+		
+		switch (_requestCode) {
+			
+			default:
+			break;
+		}
+	}
+	
+	public class Listview1Adapter extends BaseAdapter {
+		ArrayList<HashMap<String, Object>> _data;
+		public Listview1Adapter(ArrayList<HashMap<String, Object>> _arr) {
+			_data = _arr;
+		}
+		
+		@Override
+		public int getCount() {
+			return _data.size();
+		}
+		
+		@Override
+		public HashMap<String, Object> getItem(int _index) {
+			return _data.get(_index);
+		}
+		
+		@Override
+		public long getItemId(int _index) {
+			return _index;
+		}
+		@Override
+		public View getView(final int _position, View _v, ViewGroup _container) {
+			LayoutInflater _inflater = (LayoutInflater)getBaseContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			View _view = _v;
+			if (_view == null) {
+				_view = _inflater.inflate(R.layout.custom, null);
+			}
+			
+			final LinearLayout linear1 = (LinearLayout) _view.findViewById(R.id.linear1);
+			final androidx.cardview.widget.CardView cardview1 = (androidx.cardview.widget.CardView) _view.findViewById(R.id.cardview1);
+			final LinearLayout linear3 = (LinearLayout) _view.findViewById(R.id.linear3);
+			final ImageView imageview_rasm = (ImageView) _view.findViewById(R.id.imageview_rasm);
+			final LinearLayout linear2 = (LinearLayout) _view.findViewById(R.id.linear2);
+			final TextView textview_nom = (TextView) _view.findViewById(R.id.textview_nom);
+			final TextView textview_malumot = (TextView) _view.findViewById(R.id.textview_malumot);
+			
+			linear1.setBackground(new GradientDrawable() { public GradientDrawable getIns(int a, int b) { this.setCornerRadius(a); this.setColor(b); return this; } }.getIns((int)15, 0xFFFFFFFF));
+			linear1.setElevation((float)2);
+			textview_nom.setText(lsitmap_osimliklar.get((int)_position).get("nom").toString());
+			textview_malumot.setText(lsitmap_osimliklar.get((int)_position).get("malumot").toString());
+			linear1.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View _view) {
+					view_intent.setClass(getApplicationContext(), ViewActivity.class);
+					view_intent.putExtra("rasm_key", lsitmap_osimliklar.get((int)_position).get("rasm").toString());
+					view_intent.putExtra("nom_key", lsitmap_osimliklar.get((int)_position).get("nom").toString());
+					view_intent.putExtra("malumot_key", lsitmap_osimliklar.get((int)_position).get("malumot").toString());
+					startActivity(view_intent);
+				}
+			});
+			
+			return _view;
+		}
+	}
+	
+	@Deprecated
+	public void showMessage(String _s) {
+		Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Deprecated
+	public int getLocationX(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[0];
+	}
+	
+	@Deprecated
+	public int getLocationY(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[1];
+	}
+	
+	@Deprecated
+	public int getRandom(int _min, int _max) {
+		Random random = new Random();
+		return random.nextInt(_max - _min + 1) + _min;
+	}
+	
+	@Deprecated
+	public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
+		ArrayList<Double> _result = new ArrayList<Double>();
+		SparseBooleanArray _arr = _list.getCheckedItemPositions();
+		for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
+			if (_arr.valueAt(_iIdx))
+			_result.add((double)_arr.keyAt(_iIdx));
+		}
+		return _result;
+	}
+	
+	@Deprecated
+	public float getDip(int _input){
+		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, getResources().getDisplayMetrics());
+	}
+	
+	@Deprecated
+	public int getDisplayWidthPixels(){
+		return getResources().getDisplayMetrics().widthPixels;
+	}
+	
+	@Deprecated
+	public int getDisplayHeightPixels(){
+		return getResources().getDisplayMetrics().heightPixels;
+	}
+	
+}
---
+++ TopishmoqlarActivity.java
@@ -0,0 +1,157 @@
+package uz.eko.bolajon.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.*;
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.view.View.*;
+import android.widget.*;
+import android.content.*;
+import android.content.res.*;
+import android.graphics.*;
+import android.graphics.drawable.*;
+import android.media.*;
+import android.net.*;
+import android.text.*;
+import android.text.style.*;
+import android.util.*;
+import android.webkit.*;
+import android.animation.*;
+import android.view.animation.*;
+import java.util.*;
+import java.util.regex.*;
+import java.text.*;
+import org.json.*;
+import android.widget.ScrollView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import androidx.cardview.widget.CardView;
+import android.widget.ImageView;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.DialogFragment;
+
+
+public class TopishmoqlarActivity extends  AppCompatActivity  { 
+	
+	
+	private ScrollView vscroll_root_control;
+	private LinearLayout linear_root_control_1;
+	private LinearLayout linear_logo;
+	private LinearLayout linear_savol;
+	private LinearLayout linear_root_control_2;
+	private TextView textview_savol;
+	private TextView textview_logo;
+	private TextView textview_hisob;
+	private TextView textview_atribut;
+	private LinearLayout linear_round_radius_1;
+	private LinearLayout linear_round_radius_2;
+	private LinearLayout linear_round_radius_3;
+	private CardView cardview_round_radius_1;
+	private ImageView imageview_variant_1;
+	private CardView cardview_round_radius_2;
+	private ImageView imageview_variant_2;
+	private CardView cardview_round_radius_3;
+	private ImageView imageview_variant_3;
+	@Override
+	protected void onCreate(Bundle _savedInstanceState) {
+		super.onCreate(_savedInstanceState);
+		setContentView(R.layout.topishmoqlar);
+		initialize(_savedInstanceState);
+		initializeLogic();
+	}
+	
+	private void initialize(Bundle _savedInstanceState) {
+		
+		vscroll_root_control = (ScrollView) findViewById(R.id.vscroll_root_control);
+		linear_root_control_1 = (LinearLayout) findViewById(R.id.linear_root_control_1);
+		linear_logo = (LinearLayout) findViewById(R.id.linear_logo);
+		linear_savol = (LinearLayout) findViewById(R.id.linear_savol);
+		linear_root_control_2 = (LinearLayout) findViewById(R.id.linear_root_control_2);
+		textview_savol = (TextView) findViewById(R.id.textview_savol);
+		textview_logo = (TextView) findViewById(R.id.textview_logo);
+		textview_hisob = (TextView) findViewById(R.id.textview_hisob);
+		textview_atribut = (TextView) findViewById(R.id.textview_atribut);
+		linear_round_radius_1 = (LinearLayout) findViewById(R.id.linear_round_radius_1);
+		linear_round_radius_2 = (LinearLayout) findViewById(R.id.linear_round_radius_2);
+		linear_round_radius_3 = (LinearLayout) findViewById(R.id.linear_round_radius_3);
+		cardview_round_radius_1 = (CardView) findViewById(R.id.cardview_round_radius_1);
+		imageview_variant_1 = (ImageView) findViewById(R.id.imageview_variant_1);
+		cardview_round_radius_2 = (CardView) findViewById(R.id.cardview_round_radius_2);
+		imageview_variant_2 = (ImageView) findViewById(R.id.imageview_variant_2);
+		cardview_round_radius_3 = (CardView) findViewById(R.id.cardview_round_radius_3);
+		imageview_variant_3 = (ImageView) findViewById(R.id.imageview_variant_3);
+	}
+	
+	private void initializeLogic() {
+		getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+		getWindow().setStatusBarColor(0xFFFFFFFF);
+		
+		Window window = this.getWindow();window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS); window.setNavigationBarColor(Color.parseColor("#ffffff"));
+	}
+	
+	@Override
+	protected void onActivityResult(int _requestCode, int _resultCode, Intent _data) {
+		
+		super.onActivityResult(_requestCode, _resultCode, _data);
+		
+		switch (_requestCode) {
+			
+			default:
+			break;
+		}
+	}
+	
+	@Deprecated
+	public void showMessage(String _s) {
+		Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Deprecated
+	public int getLocationX(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[0];
+	}
+	
+	@Deprecated
+	public int getLocationY(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[1];
+	}
+	
+	@Deprecated
+	public int getRandom(int _min, int _max) {
+		Random random = new Random();
+		return random.nextInt(_max - _min + 1) + _min;
+	}
+	
+	@Deprecated
+	public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
+		ArrayList<Double> _result = new ArrayList<Double>();
+		SparseBooleanArray _arr = _list.getCheckedItemPositions();
+		for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
+			if (_arr.valueAt(_iIdx))
+			_result.add((double)_arr.keyAt(_iIdx));
+		}
+		return _result;
+	}
+	
+	@Deprecated
+	public float getDip(int _input){
+		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, getResources().getDisplayMetrics());
+	}
+	
+	@Deprecated
+	public int getDisplayWidthPixels(){
+		return getResources().getDisplayMetrics().widthPixels;
+	}
+	
+	@Deprecated
+	public int getDisplayHeightPixels(){
+		return getResources().getDisplayMetrics().heightPixels;
+	}
+	
+}
